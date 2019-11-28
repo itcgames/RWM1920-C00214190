b7f8a5ff0ec03a9db1dbc937983493a4e7e839f7
unknown <c00214190@itcarlow.ie>
2019-11-28 11:48:23 +0000

    Added Check so the Wrecking Ball doesn't move object without rigibodies

--- Assets/Scripts/MoveBall.cs
+++ Assets/Scripts/MoveBall.cs
@@ -16,19 +16,22 @@ public class MoveBall : MonoBehaviour
         rigidbody.angularDrag += 0.5f;
     }
 
-	// used for collision detection
+    // used for collision detection
     void OnCollisionEnter2D(Collision2D otherObject)
     {
-        Vector3 force = new Vector3(0, 0, 0)
+        if (otherObject.rigidbody != null)
         {
-            x = otherObject.transform.position.x < transform.position.x ? 0.25f : -0.25f
-        };
-        Vector2 collisionPoint = new Vector2(0, 0)
-        {
-            x = (transform.position.x + otherObject.rigidbody.position.x) / 2.0f,
-            y = (transform.position.y + otherObject.rigidbody.position.y) / 2.0f
-        };
-        otherObject.rigidbody.AddForceAtPosition(force, collisionPoint);
-        rigidbody.AddForceAtPosition(-force, collisionPoint);
+            Vector3 force = new Vector3(0, 0, 0)
+            {
+                x = otherObject.transform.position.x < transform.position.x ? 0.25f : -0.25f
+            };
+            Vector2 collisionPoint = new Vector2(0, 0)
+            {
+                x = (transform.position.x + otherObject.rigidbody.position.x) / 2.0f,
+                y = (transform.position.y + otherObject.rigidbody.position.y) / 2.0f
+            };
+            otherObject.rigidbody.AddForceAtPosition(force, collisionPoint);
+            rigidbody.AddForceAtPosition(-force, collisionPoint);
+        }
     }
 }

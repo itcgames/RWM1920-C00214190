813abe1c0e055bf8c39ad9af1962dc32474a2c4e
Cathal Redmond <C00214190@itcarlow.ie>
2020-01-20 00:03:03 +0000

    Implemented Feature 4 Adjustable Wrecking Ball, Can change starting angle and mass of ball

---
+++ Assets/ValueAdjustScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValueAdjustScript : MonoBehaviour
+{
+    [Range(0,360)]
+    public float startRotation;
+    [Range (1,15)]
+    public float ballWeight;
+    private Rigidbody2D m_ballRB2D;
+    void Start()
+    {
+        // Get Rigidbody of the Ball part of the wrecking ball
+        m_ballRB2D = transform.Find("Ball").gameObject.GetComponent<Rigidbody2D>();
+        // Set mass of object
+        m_ballRB2D.mass = ballWeight;
+        // Set rotation of entire wrecking Ball prefab
+        transform.eulerAngles = new Vector3(0, 0, startRotation);
+    }
+}
